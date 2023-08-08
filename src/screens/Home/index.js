8a53766d2148f2ed970b072b15c95508b89074d1
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2023-08-08 19:54:49 +0530

    message

---
+++ src/screens/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { SafeAreaView, View , Text} from "react-native";
+import Title from "../../components/Title";
+
+const Home = () => {
+    return(
+    <SafeAreaView>
+       <View>
+         <Title />
+       </View>
+    </SafeAreaView>
+    );
+};
+
+export default Home;
+
